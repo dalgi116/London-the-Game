2e4cb6f131f57436b87dfade6e83bdfee4faea19
Daniel Franc <daniel.franc@email.cz>
2021-02-17 14:21:49 +0100

    First edit
    Signed-off-by: Daniel Franc <daniel.franc@email.cz>

---
+++ inc/dbh.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "london-the-game";
+
+$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>London the Game</title>
+    <link rel="stylesheet" href="layouts/style.css">
+  </head>
+  <body>
+    <h1>London the Game</h1>
+    <p style="text-align:center;background-color:lightgrey">made by Daniel Franc</p>
+  </body>
+</html>
