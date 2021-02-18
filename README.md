3d2a5d2a18ae64c4484710047a29d4e0763463c7
Daniel Franc <daniel.franc@email.cz>
2021-02-18 09:46:12 +0100

    general

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# London-the-Game
+## London-the-Game
+
+- create character (rase, name, visage, element)
+- startpack (money...)
+- tutorial
+- village (arena, house, medic, shop, work, alchemist....)
+- next day
+- starving, monsters...
+- story
+
+citanka 224 - zizen po zivote
--- index.php
+++ index.php
@@ -3,10 +3,81 @@
   <head>
     <meta charset="utf-8">
     <title>London the Game</title>
-    <link rel="stylesheet" href="layouts/style.css">
+    <style media="screen">
+    h1 {
+      color: green;
+      font-size: 400%;
+      font-family: Georgia;
+    }
+    p {
+      font-size: 150%;
+      font-family: Georgia;
+      margin-left: 50px;
+    }
+    header {
+      padding: 20px;
+      text-align: center;
+    }
+    footer {
+      padding: 20px;
+      background-color: lightgrey;
+      text-align:center;
+    }
+    img {
+      padding: 50px;
+      float: right;
+      margin-right: 50px;
+    }
+    .start:link, .start:visited {
+      text-align: center;
+      font-size: 200%;
+      font-family: Georgia;
+      color: black;
+      padding: 25px 15px;
+      display: inline-block;
+      text-decoration: none;
+      background-color: #00b300;
+    }
+    .start:hover, .start:active {
+      background-color: #00cc00;
+    }
+    div {
+      text-align: center;
+    }
+
+    </style>
   </head>
   <body>
-    <h1>London the Game</h1>
-    <p style="text-align:center;background-color:lightgrey">made by Daniel Franc</p>
+    <header>
+      <h1>London the Game</h1>
+    </header>
+    <p>
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<br>
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<br>
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<br>
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<br>
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<br>
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<br>
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<br>
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<br>
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<br>
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<br>
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<br>
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<br>
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<br>
+    </p>
+    <div class="container">
+      <br><br><br>
+      <a class="start" href="#"><strong>START</strong></a>
+      <br><br><br>
+    </div>
+
+
+
+
+
+    <footer>
+      <p style="text-align:center;">made by <b>Daniel Franc</b>[<a target="_blank" style="text-decoration: none;" href="https://github.com/dalgi116"><i>dalgi116</i></a>]</p>
+    </footer>
   </body>
 </html>
